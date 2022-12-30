7753123aafbcf93c5f3aeee563a73be052c00c21
nurrizqi2201 <nurrizqi2201@gmail.com>
2022-12-30 14:43:28 +0700

    ffff

---
+++ Website Portofolio/src/components/home/Social.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home__social' data-aos="fade-right" data-aos-duration="1500">
+        <a href="https://instagram.com/" className="home__social-icon" target="_blank">
+        <i class="uil uil-instagram"></i>
+        </a>
+        
+        
+        <a href="https://facebook.com/" className="home__social-icon" target="_blank">
+        <i class="uil uil-facebook-f"></i>
+        </a>
+
+        <a href="https://github.com/" className="home__social-icon" target="_blank">
+        <i class="uil uil-github-alt"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default Social
